0a7979995f953ff47fc8fe92ce497551ccdce9c5
Fernando Scherer <fernando.scherer96@hotmail.com>
2023-07-23 11:29:59 -0300

    commits iniciais incluindo images e escrevendo as primeiras configurações do hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 
 function styles (){
-    return gulp.src('./src/styles/*.scss') // Apenas recuperamos o arquivo ainda não estamos compilando o 
-        .pipe(sass({outputStyle: 'compressed'}))
+    return gulp.src('./src/styles/*.scss') // Apenas recuperamos o arquivo ainda não estamos compilando o saa
+        .pipe(sass({outputStyle: 'compressed'})) 
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images (){
+    return gulp.src('./src/images/**/*') // Apenas recuperamos o arquivo ainda não estamos compilando 
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function (){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
