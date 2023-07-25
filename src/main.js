09e8333f6603656f26cba01f36887f120a2e0831
Fernando Scherer <fernando.scherer96@hotmail.com>
2023-07-24 23:57:54 -0300

    adicionando a section escolha seu plano

--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
+// Espera o evento 'DOMContentLoaded', que é acionado quando o DOM está completamente carregado.
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('[data-tab-button]'); // Para selecionar com base no atributo utilizamos os []
+    const buttons = document.querySelectorAll('[data-tab-button]'); // Seleciona todos os elementos com o atributo 'data-tab-button' usando o seletor '[data-tab-button]'.
 
+    // Itera sobre todos os botões obtidos anteriormente.
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao) {
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
-            escondeTodasAbas();
-            aba.classList.add('shows__list--is-active');
-            removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active')
+        buttons[i].addEventListener('click', function(botao) { // Adiciona um ouvinte de eventos de clique para cada botão.
+            const abaAlvo = botao.target.dataset.tabButton; // Obtém o valor do atributo 'data-tab-button' do botão clicado.
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`); // Seleciona a aba correspondente usando o valor obtido.
+
+            escondeTodasAbas(); // Chama a função 'escondeTodasAbas()' para ocultar todas as abas antes de exibir a desejada.
+            aba.classList.add('shows__list--is-active'); // Adiciona a classe 'shows__list--is-active' para exibir a aba desejada.
+            removeBotaoAtivo(); // Chama a função 'removeBotaoAtivo()' para remover a classe 'shows__tabs__button--is-active' de todos os botões.
+            botao.target.classList.add('shows__tabs__button--is-active'); // Adiciona a classe 'shows__tabs__button--is-active' ao botão clicado para marcá-lo como ativo.
         });
     }
 });
 
-function removeBotaoAtivo(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
+// Função que remove a classe 'shows__tabs__button--is-active' de todos os botões.
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]'); // Seleciona todos os botões com o atributo 'data-tab-button'.
 
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].classList.remove('shows__tabs__button--is-active');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active'); // Remove a classe 'shows__tabs__button--is-active' de cada botão.
     }
 }
 
+// Função que oculta todas as abas removendo a classe 'shows__list--is-active'.
 function escondeTodasAbas() {
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const tabsContainer = document.querySelectorAll('[data-tab-id]'); // Seleciona todos os elementos com o atributo 'data-tab-id'.
 
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active');
+        tabsContainer[i].classList.remove('shows__list--is-active'); // Remove a classe 'shows__list--is-active' de cada aba, ocultando-as.
     }
 }
